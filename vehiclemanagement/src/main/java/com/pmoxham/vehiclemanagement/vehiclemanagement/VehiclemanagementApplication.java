b6db35e78c8053c1c1eb9c15e4a1649947fe4729
PatrickM <patrick.moxham@gmail.com>
2025-03-17 11:51:35 +0000

    Spring initalizer project

---
+++ vehiclemanagement/src/main/java/com/pmoxham/vehiclemanagement/vehiclemanagement/VehiclemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.pmoxham.vehiclemanagement.vehiclemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclemanagementApplication.class, args);
+	}
+
+}
---
+++ vehiclemanagement/src/test/java/com/pmoxham/vehiclemanagement/vehiclemanagement/VehiclemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmoxham.vehiclemanagement.vehiclemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
